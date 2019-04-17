9dd4089fbe21251992f34525c1cd2e668772c3d6
Mateusz Musiela <mateusz.musiela@gmail.com>
2019-04-17 20:38:10 +0200

    Added registered user to vector

--- KsiazkaAdresowa.h
+++ KsiazkaAdresowa.h
@@ -1,16 +1,21 @@
 #include <iostream>
 #include <vector>
 #include "Uzytkownik.h"
+#include <windows.h>
 
 using namespace std;
 
 class KsiazkaAdresowa
 {
-        int idZalogowanegoUzytkownika;
+    int idZalogowanegoUzytkownika;
     int idOstatniegoAdresata;
     int idUsunietegoAdresata;
 
     vector <Uzytkownik> uzytkownicy;
+
+    Uzytkownik podajDaneNowegoUzytkownika();
+    int pobierzIdNowegoUzytkownika();
+    bool czyIstniejeLogin(string login);
 
 public:
     void rejestracjaUzytkownika();
